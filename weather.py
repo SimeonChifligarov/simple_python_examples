497fe9b268558218fc276529c06d1f6165ca084f
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-29 20:38:46 +0300

    improve weather.py

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 
 city = input("Въведи име на град: ")
-url = f"https://wttr.in/{city}?format=3"
+url = f"https://wttr.in/{city}"
 
 response = requests.get(url)
 print("Прогноза за времето:", response.text)
